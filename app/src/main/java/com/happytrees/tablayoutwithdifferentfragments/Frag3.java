5f2b704b0dcd68d59b72c3e62ee50f8de8e2c082
limudimwork <boriskunda@gmail.com>
2018-01-13 16:52:25 +0200

    Initial commit

---
+++ app/src/main/java/com/happytrees/tablayoutwithdifferentfragments/Frag3.java
@@ -0,0 +1,21 @@
+package com.happytrees.tablayoutwithdifferentfragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Boris on 1/13/2018.
+ */
+
+public class Frag3 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.frag3_layout,container,false);
+    }
+}
